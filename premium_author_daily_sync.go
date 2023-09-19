866760a504efd8b0b39ecc8dbf20a39b0b7689dc
Radhika Deo <radhika.deo@tifin.com>
2023-09-19 19:12:35 +0530

    Added additional logs & comments

--- premium_author_daily_sync.go
+++ premium_author_daily_sync.go
@@ -4,6 +4,7 @@ import (
 	commonUtils "author-handler-service/common"
 	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 )
@@ -11,6 +12,7 @@ import (
 func PremiumAuthorDailySync(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, "REQUEST_ID", time.Now().UnixNano())
+	log.Printf("Starting Daily sync with request ID %v", ctx.Value("REQUEST_ID"))
 	go commonUtils.RunPremiumAuthorCheck(&config, ctx)
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Completed daily sync to update premium authors")
--- simulate_followers.go
+++ simulate_followers.go
@@ -20,12 +20,14 @@ func SimulateAuthorFollowers(w http.ResponseWriter, r *http.Request) {
 	ctx = context.WithValue(ctx, "REQUEST_ID", time.Now().UnixNano())
 	vars := mux.Vars(r)
 	authorId := vars["authorId"]
+	//Geneartes random number between +100 to -100 and updates noOfFollowers in author table.
 	rand.Seed(time.Now().UnixNano())
 	randomNumber := rand.Intn(201) - 100
 	log.Printf("Changing authorId %s followers by %v with request ID %v", authorId, randomNumber, ctx.Value("REQUEST_ID"))
 	aurthorFollowerCountPayload := db.NewAuthorFollowerCountStruct(authorId, randomNumber)
 	updatedAuthorDoc, _ := db.UpdateAuthorFollowerCount(&config, aurthorFollowerCountPayload)
 	log.Printf("Simulated follower count for author %v : %v with request ID %v", authorId, updatedAuthorDoc, ctx.Value("REQUEST_ID"))
+	//Sync isPremiumAuthor condition & update the flag based on followers going up / down
 	go commonUtils.ComputeAndUpdateIsPremiumAuthor(&config, authorId, ctx)
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Simulated author's follower counts successfully!")
